6f91e3804ed2aafa4be0c04f87f589db002d86fc
kpdecker <kpdecker@gmail.com>
2012-06-27 00:19:49 -0700

    Handlebars live preview generation

--- test/style-doc.js
+++ test/style-doc.js
@@ -10,6 +10,24 @@ describe('live preview generator', function() {
     });
   });
 
+  describe('handlebars', function() {
+    it('should create rendering snippet for handlebars content', function() {
+      styleDoc('```handlebars\n{{template "foo"}}```').should.equal(
+          '<pre><code class="lang-handlebars">{{template &quot;foo&quot;}}</code></pre>\n'
+          + '<div id="handlebars-1" class="style-doc-sample"></div>\n'
+          + '<script>document.getElementById("handlebars-1").innerHTML = Handlebars.compile("{{template \\"foo\\"}}")();</script>');
+    });
+    it('should create unique ids for each handlebars entry', function() {
+      styleDoc('```handlebars\n{{template "foo"}}```\n```handlebars\n{{template "bar"}}```').should.equal(
+          '<pre><code class="lang-handlebars">{{template &quot;foo&quot;}}</code></pre>\n'
+          + '<div id="handlebars-1" class="style-doc-sample"></div>\n'
+          + '<script>document.getElementById("handlebars-1").innerHTML = Handlebars.compile("{{template \\"foo\\"}}")();</script>'
+          + '<pre><code class="lang-handlebars">{{template &quot;bar&quot;}}</code></pre>\n'
+          + '<div id="handlebars-2" class="style-doc-sample"></div>\n'
+          + '<script>document.getElementById("handlebars-2").innerHTML = Handlebars.compile("{{template \\"bar\\"}}")();</script>');
+    });
+  });
+
   describe('unknown code', function() {
     it('should ignore code blocks without a language type', function() {
       styleDoc('```\n{{template "foo"}}```').should.equal(
