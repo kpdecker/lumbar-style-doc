2b380cb6e4c5143fd83e80839d064cb991febb8c
kpdecker <kpdecker@gmail.com>
2012-06-27 00:11:54 -0700

    html live preview generation

--- test/style-doc.js
+++ test/style-doc.js
@@ -2,6 +2,14 @@ var styleDoc = require('../lib/style-doc'),
     should = require('should');
 
 describe('live preview generator', function() {
+  describe('html', function() {
+    it('should create live preview for html content', function() {
+      styleDoc('```html\n<div class="foo">Test</div>```').should.equal(
+          '<pre><code class="lang-html">&lt;div class=&quot;foo&quot;&gt;Test&lt;/div&gt;</code></pre>\n'
+          + '<div class="style-doc-sample"><div class="foo">Test</div></div>');
+    });
+  });
+
   describe('unknown code', function() {
     it('should ignore code blocks without a language type', function() {
       styleDoc('```\n{{template "foo"}}```').should.equal(
