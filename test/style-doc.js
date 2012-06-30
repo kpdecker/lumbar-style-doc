3b5f0fad6be6baa8448404bd3f205e48050a102f
kpdecker <kpdecker@gmail.com>
2012-06-30 11:33:35 -0500

    Fix highlighter escaping issue for html

--- test/style-doc.js
+++ test/style-doc.js
@@ -35,7 +35,7 @@ describe('live preview generator', function() {
 
     it('should highlight', function() {
       styleDoc('```html\n<div>Test</div>```', {template: false}).should.equal(
-          '<pre><code class="lang-html"><span class="tag">&lt;<span class="title">div</span>></span>Test<span class="tag">&lt;/<span class="title">div</span>></span></code></pre>\n'
+          '<pre><code class="lang-html"><span class="tag">&lt;<span class="title">div</span>&gt;</span>Test<span class="tag">&lt;/<span class="title">div</span>&gt;</span></code></pre>\n'
           + '<div class="style-doc-sample"><div>Test</div></div>');
     });
   });
