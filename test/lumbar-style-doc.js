0d6982515ff0c938beaa272f26aeb92791a80fa5
kpdecker <kpdecker@gmail.com>
2012-07-04 22:35:47 -0500

    Test coverage for resource includes

--- test/lumbar-style-doc.js
+++ test/lumbar-style-doc.js
@@ -2,7 +2,6 @@ var assert = require('assert'),
     lib = require('../node_modules/lumbar/test/lib'),
     styleDoc = require('../lib/lumbar-style-doc');
 
-
 describe('config', function() {
   it('should load markdown file from config',
     lib.runTest('test/artifacts/style-doc.json', 'test/expected/style-doc', {plugins: [styleDoc]}, '/**/*.{js,css,html}'));
