cb6a74e49b3e256c6654aa59e2512602b6202214
kpdecker <kpdecker@gmail.com>
2012-07-05 13:38:06 -0500

    Watch tests

--- test/lumbar-style-doc.js
+++ test/lumbar-style-doc.js
@@ -1,8 +1,47 @@
-var assert = require('assert'),
-    lib = require('../node_modules/lumbar/test/lib'),
-    styleDoc = require('../lib/lumbar-style-doc');
+var lib = require('../node_modules/lumbar/test/lib'),
+    styleDoc = require('../lib/lumbar-style-doc'),
+    watch = require('./lib/watch');
 
 describe('config', function() {
   it('should load markdown file from config',
     lib.runTest('test/artifacts/style-doc.json', 'test/expected/style-doc', {plugins: [styleDoc]}, '/**/*.{js,css,html}'));
+});
+
+describe('watch', function() {
+  if (!watch.canWatch()) {
+    return;
+  }
+
+  it('should rebuild on input changes', function(done) {
+    this.timeout(10000);
+
+    var expectedFiles = [
+            '/bacon/base.css', '/bacon/base.js', '/bacon/items.css', '/bacon/style-doc.html',
+                '/ribs/base.css', '/ribs/base.js', '/ribs/items.css', '/ribs/style-doc.html',
+            '/bacon/base.css', '/bacon/base.js', '/bacon/items.css', '/bacon/style-doc.html',
+                '/ribs/base.css', '/ribs/base.js', '/ribs/items.css', '/ribs/style-doc.html',
+            '/bacon/style-doc.html', '/ribs/style-doc.html',
+            '/bacon/style-doc.html', '/ribs/style-doc.html'
+          ],
+        operations = {
+          8: function(testdir) {
+            // Modify the config file
+            watch.appendSpace(testdir + '/style-doc.json');
+          },
+          16: function(testdir) {
+            // Modify the bridge file
+            watch.appendSpace(testdir + '/style-doc.md');
+          },
+          18: function(testdir) {
+            // Modify the home template
+            watch.appendSpace(testdir + '/template.handlebars');
+          }
+        };
+
+    this.title = 'watch';
+    watch.runWatchTest.call(this,
+      'test/artifacts', 'style-doc.json',
+      operations, expectedFiles, {plugins: [styleDoc]},
+      done);
+  });
 });
