7b83c9ac0f32dd85b1604864e853bd079b2160bb
kpdecker <kpdecker@gmail.com>
2012-07-05 11:19:35 -0500

    Template helper

--- test/expected/style-doc/bacon/base.js
+++ test/expected/style-doc/bacon/base.js
@@ -2906,9 +2906,31 @@ Handlebars.VM = {
 Handlebars.template = Handlebars.VM.template;
 ;
 ;;
+Handlebars.templates = Handlebars.templates || {};
+window.context = {};
+
+Handlebars.registerHelper('template', function(name, options) {
+  var data = options.hash || {};
+  for (var propName in this) {
+    if (this.hasOwnProperty(propName) && !(propName in data)) {
+      data[propName] = this[propName];
+    }
+  }
+
+  data.yield = function() {
+    return options.fn && options.fn(data);
+  };
+
+  var template = Handlebars.templates[name];
+  if (!template) {
+    throw new Error('Unable to find template ' + name);
+  } else {
+    return new Handlebars.SafeString(template(data));
+  }
+});
+
 $(document).ready(function() {
   $('body > section').addClass('doc-tabbed');
-
 
   var tabs = $('[data-tab]').bind('click', function(event) {
       $('.doc-active').removeClass('doc-active');
