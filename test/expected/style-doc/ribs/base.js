e29c940f9f3aa393c14b68a7476f20685befc243
kpdecker <kpdecker@gmail.com>
2012-07-05 00:13:17 -0500

    Link navbar to hash

--- test/expected/style-doc/ribs/base.js
+++ test/expected/style-doc/ribs/base.js
@@ -2909,13 +2909,29 @@ Handlebars.template = Handlebars.VM.template;
 $(document).ready(function() {
   $('body > section').addClass('doc-tabbed');
 
-  $('[data-tab]').bind('click', function(event) {
+
+  var tabs = $('[data-tab]').bind('click', function(event) {
       $('.doc-active').removeClass('doc-active');
       $(event.currentTarget.getAttribute('data-tab')).addClass('doc-active');
       $(event.currentTarget).addClass('doc-active');
+
+      location.hash = event.currentTarget.getAttribute('data-text');
     })
-    .first()
-    .trigger('click');
+    .each(function() {
+      this.setAttribute('data-text', this.innerText);
+    });
+
+  function updateTab() {
+    var hash = location.hash.replace(/^#/, '');
+    if (!hash) {
+      tabs.trigger('click');
+    } else {
+      $('[data-text="' + hash + '"]').trigger('click');
+    }
+  }
+
+  updateTab();
+  $(window).bind('hashchange', updateTab);
 });
 ;;
 base = (function() {
